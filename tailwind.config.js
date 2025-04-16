5eec503820a2fbc893d52533d265b462ccbfd109
Mateus Acauhi <mateusacauhi@bluems.com>
2025-04-15 22:25:49 -0300

    feat: limpar coisas desnecessarias projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{razor,html,cshtml,css}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-        display: ["Special Gothic Condensed One", "sans-serif"],
-        mono: ["Fira Code", "monospace"],
-      },
-      colors: {
-        glass: "rgba(255, 255, 255, 0.2)",
-      },
-    },
-  },
+  content: ['./**/*.{razor,html,cshtml,css}'],
+  theme: {},
   plugins: [],
 };
